477daec86d7e7822a1bd6bff34ff102e360f498f
Uelinton <79346985+uelintonn@users.noreply.github.com>
2024-05-04 10:37:09 -0300

    Initial Commit

---
+++ FoodPin/RestaurantListView.swift
@@ -0,0 +1,140 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 21/9/2021.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    var restaurantNames = ["Uelinton Coffee", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var restaurantLocations = ["Brazil - Maceió", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+        
+    var restaurantTypes = ["Café e Shoppe", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var body: some View {
+        List {
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                BasicTextImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index]) //aqui foi substituido o BasicTextImageRow por FullImageRow
+            }
+            
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        
+    }
+}
+
+struct RestaurantListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantListView()
+        
+        RestaurantListView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct BasicTextImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    
+    @State private var showOptions = true
+    @State private var showError = false
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .frame(width: 120, height: 118)
+                .cornerRadius(20)
+            
+        }
+        
+        VStack(alignment: .leading) {
+            Text(name)
+                .font(.system(.title2, design: .rounded))
+            
+            Text(type)
+                .font(.system(.body, design: .rounded))
+            
+            Text(location)
+                .font(.system(.subheadline, design: .rounded))
+                .foregroundColor(.yellow)
+        }
+        .onTapGesture {
+            showOptions.toggle()
+        }
+        .actionSheet(isPresented: $showOptions) {
+            ActionSheet(title: Text ("O que você deseja fazer?"),
+                        message: nil,
+                        buttons: [
+                            .default(Text("1 - Reservar uma mesa")){
+                                self.showError.toggle()
+                            },
+                            .default(Text("2 - Marcar uma mesa favorita")) {
+                                
+                            },
+                            .cancel()
+                        ])
+            
+            .alert(isPresented: $showError) {
+                Alert(title: Text("Não está disponível"),
+                      message: Text("Sorry, está função não está disponível, por favor tente mais tarde."),
+                      
+                      primaryButton: .default(Text("OK")),
+                      secondaryButton: .cancel())
+                
+                
+                
+                
+            }
+        }
+    }
+}
+
+
+/*struct FullImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .scaledToFill()
+                .frame(height: 200)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                    
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.yellow)
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            
+            Text("Escrito por Uelinton") // Só pra colocar meu nome no projeto kkkkkkkkkkk
+                .font(.callout)
+                .foregroundColor(.yellow)
+        }
+    }
+}
+*/
---
+++ Restaurant.swift
@@ -0,0 +1,27 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Uelinton Andrade on 04/05/24.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isFavorite: Bool
+    
+    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    init() {
+        self.init(name:"",type: "", location: "", image: "", isFavorite: false)
+    }
+}
