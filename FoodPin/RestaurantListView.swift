4e1e4809bcccc09f94f12c99e20a953db9687ab5
Uelinton <79346985+uelintonn@users.noreply.github.com>
2024-05-07 13:43:27 -0300

    Finalizando a primeira parte

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -2,24 +2,42 @@
 //  ContentView.swift
 //  FoodPin
 //
-//  Created by Simon Ng on 21/9/2021.
+//  Created by Uelinton Andrade 04/05/24.
 //
 
 import SwiftUI
 
 struct RestaurantListView: View {
-    var restaurantNames = ["Uelinton Coffee", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    @State var restaurants = [
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location:"Hong Kong", image: "cafedeadend", isFavorite: false),
+        Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image:"homei", isFavorite: false),
+        Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image: "teakha", isFavorite: false),
+        Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location: "Hong Kong", image: "cafeloisl",  isFavorite: false),
+        Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong", image: "petiteoyster", isFavorite: false),
+        Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "HongKong", image: "forkee", isFavorite: false),
+        Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong", image: "posatelier", isFavorite: false),
+        Restaurant(name: "Bourke Street Backery", type: "Chocolate", location: "Sydney", image: "bourkestreetbakery", isFavorite: false),
+        Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney", image: "haigh", isFavorite: false),
+        Restaurant(name: "Palomino Espresso", type: "American / Seafood", location: "Sydney", image: "palomino", isFavorite: false),
+        Restaurant(name: "Upstate", type: "American", location: "New York", image: "upstate", isFavorite: false),
+        Restaurant(name: "Traif", type: "American", location: "New York", image: "traif", isFavorite: false),
+        Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch", location: "New York", image: "graham", isFavorite: false),
+        Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "New York", image: "waffleandwolf", isFavorite: false),
+        Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York", image: "fiveleaves", isFavorite: false),
+        Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York", image: "cafelore", isFavorite: false),
+        Restaurant(name: "Confessional", type: "Spanish", location: "New York", image: "confessional", isFavorite: false),
+        Restaurant(name: "Barrafina", type: "Spanish", location: "London", image: "barrafina", isFavorite: false),
+        Restaurant(name: "Donostia", type: "Spanish", location: "London", image: "donostia", isFavorite: false),
+        Restaurant(name: "Royal Oak", type: "British", location: "London", image: "royaloak", isFavorite: false),
+        Restaurant(name: "CASK Pub and Kitchen", type: "Thai", location: "London", image: "cask", isFavorite: false)
+    ]
     
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
     
-    var restaurantLocations = ["Brazil - Maceió", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-        
-    var restaurantTypes = ["Café e Shoppe", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
     var body: some View {
         List {
-            ForEach(restaurantNames.indices, id: \.self) { index in
-                BasicTextImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index]) //aqui foi substituido o BasicTextImageRow por FullImageRow
+            ForEach(restaurants.indices, id: \.self) { index in
+                BasicTextImageRow(restaurant: $restaurants[index])
             }
             
             .listRowSeparator(.hidden)
@@ -40,96 +58,73 @@ struct RestaurantListView_Previews: PreviewProvider {
 
 struct BasicTextImageRow: View {
     
-    var imageName: String
-    var name: String
-    var type: String
-    var location: String
+    @Binding var restaurant: Restaurant
     
     
-    @State private var showOptions = true
+    @State private var showOptions = false
     @State private var showError = false
     
     var body: some View {
         HStack(alignment: .top, spacing: 20) {
-            Image(imageName)
+            Image(restaurant.image)
                 .resizable()
                 .frame(width: 120, height: 118)
                 .cornerRadius(20)
             
-        }
-        
-        VStack(alignment: .leading) {
-            Text(name)
-                .font(.system(.title2, design: .rounded))
             
-            Text(type)
-                .font(.system(.body, design: .rounded))
-            
-            Text(location)
-                .font(.system(.subheadline, design: .rounded))
-                .foregroundColor(.yellow)
-        }
-        .onTapGesture {
-            showOptions.toggle()
-        }
-        .actionSheet(isPresented: $showOptions) {
-            ActionSheet(title: Text ("O que você deseja fazer?"),
-                        message: nil,
-                        buttons: [
-                            .default(Text("1 - Reservar uma mesa")){
-                                self.showError.toggle()
-                            },
-                            .default(Text("2 - Marcar uma mesa favorita")) {
-                                
-                            },
-                            .cancel()
-                        ])
-            
-           
-                
-                
-                
-                
-            }
-        }
-    }
-
-
-
-/*struct FullImageRow: View {
-    
-    var imageName: String
-    var name: String
-    var type: String
-    var location: String
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .scaledToFill()
-                .frame(height: 200)
-                .cornerRadius(20)
             
             VStack(alignment: .leading) {
-                Text(name)
+                Text(restaurant.name)
                     .font(.system(.title2, design: .rounded))
-                    
-                Text(type)
+                
+                Text(restaurant.type)
                     .font(.system(.body, design: .rounded))
                 
-                Text(location)
+                Text(restaurant.location)
                     .font(.system(.subheadline, design: .rounded))
                     .foregroundColor(.yellow)
             }
-            .padding(.horizontal)
-            .padding(.bottom)
             
-            Text("Escrito por Uelinton") // Só pra colocar meu nome no projeto kkkkkkkkkkk
-                .font(.callout)
-                .foregroundColor(.yellow)
+            
+            if restaurant.isFavorite {
+                Spacer()
+                
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.yellow)
+            }
         }
+            .onTapGesture {
+                showOptions.toggle()
+            }
+            .actionSheet(isPresented: $showOptions) {
+                ActionSheet(title: Text ("O que você deseja fazer?"),
+                            message: nil,
+                            buttons: [
+                                .default(Text("1 - Reservar uma mesa")){
+                                    self.showError.toggle()
+                                },
+                                .default(Text(restaurant.isFavorite ? "Desmarcar Favorita ": "2 - Marque como favorita")) {
+                                    self.restaurant.isFavorite.toggle()
+                                },
+                                .cancel()
+                            ])
+            }
+                .alert(isPresented: $showError) {
+                    Alert (title: Text("Não está disponível"),
+                           message: Text ("Desculpa esta função não está disponivel ainda, por favor volte depois."),
+                           primaryButton: .default(Text("OK")),
+                           secondaryButton: .cancel())
+                }
+                
+                
+                
+            
+                
+            }
+        
     }
-}
-*/
+
+
+
+
+
